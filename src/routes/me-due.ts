61338bbeea513a8636b57a362628ee1b77bab8ee
moyshik7 <moyshik7@gmail.com>
2021-07-12 23:28:29 +0600

    completed me-due (GET)

--- src/routes/me-due.ts
+++ src/routes/me-due.ts
@@ -1,25 +1,20 @@
 import { Request, Response } from 'express'
-import { User, Snowflake } from './../typings'
+import { Invoice, Snowflake } from './../typings'
 import { Database } from './../res/db'
 
-export const Me = (app: any, db: Database): void => {
-    app.get('/me', (req: Request, res: Response): void => {
+export const Me_due = (app: any, db: Database): void => {
+    app.get('/me/due', (req: Request, res: Response): void => {
         /**
          * Error 400: Bad request 
          */
         if(!req.headers.authorization) { return res.status(403).json({ code: 403, error: "No auth token or invalid auth token" }) }
         db.GetUserByToken(req.headers.authorization).then((_u: Snowflake | null): void => {
             if(!_u){ return res.status(403).json({ code: 403, error: "No auth token or invalid auth token" }) }
-            db.GetUserByID(_u).then((_user: User | null) => {
+            db.GetIncompleteInvoices(_u).then((_inv: Invoice[] | any[]) => {
                 /**
-                 * Now there's possibly no chance of the user returning null 
-                 * but just to be safe 
+                 * If everything goes well we'll return the invoices
                  */
-                if(!_user){ return res.status(404).json({ code: 404, error: "User not found or deleted" }) }
-                /**
-                 * If everything goes well we'll return the user 
-                 */
-                return res.status(200).json({ code: 200, user: _user })
+                return res.status(200).json({ code: 200, invoices: _inv })
             })
             /**
              * In case something goes wrong 
