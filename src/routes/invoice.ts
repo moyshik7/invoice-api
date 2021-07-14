bdec5e991322227d435980495d03385154946127
moyshik7 <moyshik7@gmail.com>
2021-07-14 13:28:45 +0600

    Added /invoice (get) route

---
+++ src/routes/invoice.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from 'express'
+import { Invoice, Snowflake } from './../typings'
+import { Database } from './../res/db'
+
+export const Invoice = (app: any, db: Database): void => {
+    app.get('/invoice', (req: Request, res: Response): void => {
+        /**
+         * Error 400: Bad request 
+         */
+        if(!req.headers.authorization) { return res.status(403).json({ code: 403, error: "No auth token or invalid auth token" }) }
+        db.GetUserByToken(req.headers.authorization).then((_u: Snowflake | null): void => {
+            if(!_u){ return res.status(403).json({ code: 403, error: "No auth token or invalid auth token" }) }
+            db.GetInvoiceByUser(_u).then((_inv: Invoice[] | any[]) => {
+                /**
+                 * If everything goes well we'll return the invoices
+                 */
+                return res.status(200).json({ code: 200, invoices: _inv })
+            })
+            /**
+             * In case something goes wrong 
+             */
+            .catch((err: any):void => {  // eslint-disable-line @typescript-eslint/no-unused-vars
+                /**
+                 * Error 500: Internal server error 
+                 */
+                res.status(500).json({ code: 500, error: "Something Went Wrong on our side" })
+            })
+        })
+        /**
+         * If database fails or something like that 
+         */
+        .catch((err: any):void => {  // eslint-disable-line @typescript-eslint/no-unused-vars
+            /**
+             * Error 500: Internal server error 
+             */
+            res.status(500).json({ code: 500, error: "Something Went Wrong on our side" })
+        })
+        
+    })
+}
