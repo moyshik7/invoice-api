bd4f087103871396e14aa18b376677d26133b522
moyshik7 <moyshik7@gmail.com>
2021-07-14 13:12:37 +0600

    Added me/pings path to main file

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,7 @@ import { Login } from './routes/login'
 import { Signup } from './routes/signup'
 import { Me } from './routes/me'
 import { Me_due } from './routes/me-due'
+import { Me_pings } from './routes/me-pings'
 import { Invoice_id } from './routes/invoice_id'
 
 const app = new express()
@@ -47,6 +48,7 @@ Database.connect(DB_URL, DB_NAME).then((db: Database): void => {
     Signup(app, db)
     Me(app, db)
     Me_due(app, db)
+    Me_pings(app, db)
     Invoice_id(app, db)
 
     app.listen(PORT, (): void => {
