cdf3e48b74d90d38b9674ecc596c47f700414df5
moyshik7 <moyshik7@gmail.com>
2021-07-15 16:48:21 +0600

    Added update user option

--- src/routes/me.ts
+++ src/routes/me.ts
@@ -40,6 +40,44 @@ export const Me = (app: any, db: Database): void => {
              */
             res.status(500).json({ code: 500, error: "Something Went Wrong on our side" })
         })
-        
+    })
+    app.post('/me', (req: Request, res: Response): void => {
+        /**
+         * Error 400: Bad request 
+         * Error 403: Access denied
+         */
+        if(!req.headers.authorization) { return res.status(403).json({ code: 403, error: "No auth token or invalid auth token" }) }
+        if(!req.body.user|| req.body.user == {}){ return res.status(400).json({ code: 400, error: "provide a valid User to update" }) }
+        db.GetUserByToken(req.headers.authorization).then(async (_u: Snowflake | null): Promise<void> => {
+            if(!_u){ return res.status(403).json({ code: 403, error: "No auth token or invalid auth token" }) }
+            const old_usr: User | null = await db.GetUserByID(_u)
+            if(!old_usr){ return res.status(404).json({ code: 404, error: "This user is not found or might have been deleted" }) }
+            
+            const update: any = {}
+            if(req.body.user.name && req.body.invoice.name !== old_usr.name){ update.name = req.body.user.name }
+            
+            if(!update || update == {}){ return res.status(200).json({ code: 200, message: "Updated" }) }
+            db.UpdateUser(_u, update).then((): void => {
+                res.status(200).json({ code: 200, message: "Updated" })
+            })
+            /**
+             * In case something goes wrong 
+             */
+            .catch((err: any):void => {  // eslint-disable-line @typescript-eslint/no-unused-vars
+                /**
+                 * Error 500: Internal server error 
+                 */
+                res.status(500).json({ code: 500, error: "Something Went Wrong on our side" })
+            })
+        })
+        /**
+         * If database fails or something like that 
+         */
+        .catch((err: any):void => {  // eslint-disable-line @typescript-eslint/no-unused-vars
+            /**
+             * Error 500: Internal server error 
+             */
+            res.status(500).json({ code: 500, error: "Something Went Wrong on our side" })
+        })
     })
 }
