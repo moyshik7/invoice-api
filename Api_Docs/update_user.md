7d57e0b2709dd8e363766dea3d1867bf54c3ff0b
moyshik7 <moyshik7@gmail.com>
2021-07-15 17:03:05 +0600

    Trying to fix /me (POST)

---
+++ Api_Docs/update_user.md
@@ -0,0 +1,12 @@
+# Update your information
+`/me` (POST)  
+  
+  
+Send a POST request to `/me` with user object which contains the details to be updated  
+**(Authorization required)**  
+  
+**Example:**  
+
+```json
+
+```
--- src/routes/me.ts
+++ src/routes/me.ts
@@ -47,16 +47,16 @@ export const Me = (app: any, db: Database): void => {
          * Error 403: Access denied
          */
         if(!req.headers.authorization) { return res.status(403).json({ code: 403, error: "No auth token or invalid auth token" }) }
-        if(!req.body.user|| req.body.user == {}){ return res.status(400).json({ code: 400, error: "provide a valid User to update" }) }
+        if(!req.body.user || req.body.user == {}){ return res.status(400).json({ code: 400, error: "provide a valid User to update" }) }
         db.GetUserByToken(req.headers.authorization).then(async (_u: Snowflake | null): Promise<void> => {
             if(!_u){ return res.status(403).json({ code: 403, error: "No auth token or invalid auth token" }) }
             const old_usr: User | null = await db.GetUserByID(_u)
             if(!old_usr){ return res.status(404).json({ code: 404, error: "This user is not found or might have been deleted" }) }
             
             const update: any = {}
-            if(req.body.user.name && req.body.invoice.name !== old_usr.name){ update.name = req.body.user.name }
+            if(req.body.user.name && req.body.user.name !== old_usr.name){ update.name = req.body.user.name }
             
-            if(!update || update == {}){ return res.status(200).json({ code: 200, message: "Updated" }) }
+            if(JSON.stringify(update) == "{}"){ return res.status(200).json({ code: 200, message: "Updated" }) }
             db.UpdateUser(_u, update).then((): void => {
                 res.status(200).json({ code: 200, message: "Updated" })
             })
@@ -68,6 +68,7 @@ export const Me = (app: any, db: Database): void => {
                  * Error 500: Internal server error 
                  */
                 res.status(500).json({ code: 500, error: "Something Went Wrong on our side" })
+                console.log(err)
             })
         })
         /**
@@ -78,6 +79,7 @@ export const Me = (app: any, db: Database): void => {
              * Error 500: Internal server error 
              */
             res.status(500).json({ code: 500, error: "Something Went Wrong on our side" })
+            console.log(err)
         })
     })
 }
